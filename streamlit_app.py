0488678ee0936baeffb816658d2003d41a806a9a
Kalenak <104581750+Kalenak@users.noreply.github.com>
2022-05-18 16:45:26 +0100

    Update streamlit_app.py
    Changed header, added header, added new menu item, added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
 
-streamlit.title('My Dad''s Healthy Diner')
+streamlit.title('My Dad\'s Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free Range Eggs')
+streamlit.header('Breakfast Favourites')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal🥣')
+streamlit.text('Kale, Spinach & Rocket Smoothie🥗')
+streamlit.text('Hard-Boiled Free Range Eggs🐔')
+streamlit.text('Avocado on Toast🥑')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
